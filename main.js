8855f03a9f075bc5982f32fb38e9b03fe3b5864f
Charles Woodring <charles.s.woodring@gmail.com>
2018-09-06 19:57:12 -0500

    initial files

---
+++ main.js
@@ -0,0 +1,8 @@
+var language = [ 
+    { 
+        english: "Wishing you a harvest of blessings, good health and good times. Happy Thanksgiving day!",
+        spanish: "Te deseo una cosecha de bendiciones, buena salud y buenos momentos. ¡Feliz día de acción de gracias!",
+        french: "Je vous souhaite une récolte de bénédictions, une bonne santé et de bons moments. Bonne fête de Thanksgiving!",
+        italian: "Vi auguro un raccolto di benedizioni, buona salute e bei tempi. Felice giorno del Ringraziamento!"
+    },
+]
